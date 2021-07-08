a739e4a05a2825d67c635f67e9c85c5dd6ba2111
Satyam Dorville <satyam.dorville@gmail.com>
2021-07-08 02:39:40 -0400

    eject

--- android/app/src/main/java/com/satyamdorville/discover/MainActivity.java
+++ android/app/src/main/java/com/satyamdorville/discover/MainActivity.java
@@ -37,13 +37,15 @@ public class MainActivity extends ReactActivity {
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
+    @Override
     protected String getMainComponentName() {
-        return "Table Discover";
+        return "main";
     }
 
+    @Override
     protected ReactActivityDelegate createReactActivityDelegate() {
         return new ReactActivityDelegate(this, getMainComponentName()) {
-
+            @Override
             protected ReactRootView createRootView() {
                 return new RNGestureHandlerEnabledRootView(MainActivity.this);
             }
